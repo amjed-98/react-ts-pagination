3a6c3c7d2fa2af4915623586e3ecd71d2f15b7d1
= <=>
2022-10-25 11:48:28 +0300

    add better types for event file

--- src/lib/utils/event.ts
+++ src/lib/utils/event.ts
@@ -1,18 +1,20 @@
 type EventType = keyof DocumentEventMap | 'pageChange';
-type Listener = (event: CustomEvent<number>) => void;
+type Listener<Payload> = (event: CustomEvent<Payload>) => void;
 
-const listenFor = (eventType: EventType, listener: Listener): (() => void) => {
-  document.addEventListener(eventType, listener as never);
-  return () => document.removeEventListener(eventType, listener as never);
+const listenFor = <Payload>(eventType: EventType, listener: Listener<Payload>): (() => void) => {
+  document.addEventListener(eventType, listener as EventListener);
+
+  return () => document.removeEventListener(eventType, listener as EventListener);
 };
 
 const dispatchEvent = <Payload>(eventType: EventType, payload: Payload): void => {
   const event = new CustomEvent<Payload>(eventType, { detail: payload });
+
   document.dispatchEvent(event);
 };
 
-const unSubscribe = (eventType: EventType, listener: Listener): void => {
-  document.removeEventListener(eventType, listener as never);
+const unSubscribe = <Payload>(eventType: EventType, listener: Listener<Payload>): void => {
+  document.removeEventListener(eventType, listener as EventListener);
 };
 
 export { listenFor, dispatchEvent, unSubscribe };
