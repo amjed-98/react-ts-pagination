535dd1d7d0277da10efe0ebdf56a8d26e7bcc75a
= <=>
2022-10-29 16:48:05 +0200

    add two demos components for how to use the package: 1- one with the usePagination hook 1- one without the usePagination hook

---
+++ src/demo/Without-usePagination-hook.tsx
@@ -0,0 +1,50 @@
+import { useState, useRef, useEffect } from 'react';
+import items from './items';
+import { Pagination } from '@/lib';
+import Table from './Table';
+
+const ITEMS_PER_PAGE = 10;
+const numberOfPages = Math.ceil(items.length / ITEMS_PER_PAGE);
+
+function Without_usePagination_Hook() {
+  const [pageItems, setPageItems] = useState<typeof items>([]);
+  const currentPageNumber = useRef(1);
+
+  const handlePageChange = (pageNumber: number, pageRef: HTMLSpanElement | undefined) => {
+    const FIRST_PAGE_NUMBER = 1;
+    const LAST_PAGE_NUMBER = numberOfPages;
+
+    const isFirstPage = pageNumber + 1 === FIRST_PAGE_NUMBER;
+    const isLastPage = pageNumber - 1 === LAST_PAGE_NUMBER;
+
+    if (isLastPage || isFirstPage) return;
+
+    const start = (pageNumber - 1) * ITEMS_PER_PAGE;
+    const end = pageNumber * ITEMS_PER_PAGE;
+
+    currentPageNumber.current = pageNumber;
+    setPageItems(items.slice(start, end));
+
+    pageRef?.scrollIntoView();
+  };
+
+  useEffect(() => {
+    const start = (currentPageNumber.current - 1) * ITEMS_PER_PAGE;
+    const end = currentPageNumber.current * ITEMS_PER_PAGE;
+    setPageItems(items.slice(start, end));
+  }, []);
+
+  return (
+    <div className='App'>
+      <Table pageItems={pageItems} />
+
+      <Pagination
+        currentPageNumber={currentPageNumber.current}
+        numberOfPages={numberOfPages}
+        onPageChange={handlePageChange}
+      />
+    </div>
+  );
+}
+
+export default Without_usePagination_Hook;
--- src/demo/index.tsx
+++ src/demo/index.tsx
@@ -1,69 +1,11 @@
-import React from 'react';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import items from './items';
-import { usePagination, Pagination } from '@/lib';
-import './index.css';
-
-const Table = ({ pageItems }: { pageItems: typeof items }) => {
-  const tableHeaders = ['id', 'First name', 'Last name', 'email', 'Phone'];
-  return (
-    <table>
-      <thead>
-        <tr>
-          {tableHeaders.map((header) => (
-            <th key={header}>{header}</th>
-          ))}
-        </tr>
-      </thead>
-
-      <tbody>
-        {pageItems.map(({ id, first_name, last_name, email, phone }) => {
-          return (
-            <tr key={id}>
-              <td>{id}</td>
-              <td>{first_name}</td>
-              <td>{last_name}</td>
-              <td>{email}</td>
-              <td>{phone}</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
-  );
-};
-
-export default Table;
-
-function App() {
-  const { currentPageNumber, numberOfPages, pageItems } = usePagination({
-    items,
-  });
-
-  return (
-    <div className='App'>
-      <Table pageItems={pageItems} />
-
-      <Pagination
-        currentPageNumber={currentPageNumber}
-        numberOfPages={numberOfPages}
-        // activePageClass='active'
-        // pageClass='page'
-        // nextLabel={'next'}
-        // prevLabel={'prev'}
-        // pageStyle={{ backgroundColor: 'cyan', color: 'green' }}
-        // activePageStyle={{ backgroundColor: 'red', color: 'blue' }}
-        // paginationContainerClass='container'
-        // nextBtnClass='next'
-        // prevBtnClass='prev'
-        // pagesContainerClass='page-container'
-      />
-    </div>
-  );
-}
+import With_usePagination_Hook from './With-usePagination-hook';
+import Without_usePagination_Hook from './Without-usePagination-hook';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <StrictMode>
+    <With_usePagination_Hook />
+    {/* <Without_usePagination_Hook /> */}
+  </StrictMode>,
 );
