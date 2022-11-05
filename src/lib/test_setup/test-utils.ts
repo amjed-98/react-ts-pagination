dda91d835f2fe73d389f132c21898e5b8fdcbd1a
= <=>
2022-11-05 08:41:30 +0200

    add setupServer utils function in a separate file

--- src/lib/test_setup/test-utils.ts
+++ src/lib/test_setup/test-utils.ts
@@ -1,34 +1,12 @@
 import { cleanup, render, RenderOptions } from '@testing-library/react';
 import { afterEach } from 'vitest';
-import { rest } from 'msw';
-import { setupServer, type SetupServerApi } from 'msw/node';
-import DUMMY_ITEMS from './DUMMY_ITEMS';
 
 afterEach(cleanup);
 
 const customRender = (ui: React.ReactElement, options: RenderOptions = {}) => render(ui, options);
 export const arrayOf = (length: number) => Array.from({ length });
 
-type Parameters = {
-  endpoint: string;
-  pageNumber?: number;
-  itemsPerPage?: number;
-};
-
-const createServer = ({
-  endpoint,
-  pageNumber,
-  itemsPerPage = DUMMY_ITEMS.length,
-}: Parameters): SetupServerApi => {
-  const start = pageNumber ? (pageNumber - 1) * itemsPerPage : 0;
-  const end = pageNumber ? pageNumber * itemsPerPage : itemsPerPage;
-  const response = DUMMY_ITEMS.slice(start, end);
-
-  return setupServer(rest.get(endpoint, (_req, res, ctx) => res(ctx.json(response))));
-};
-
 export * from '@testing-library/react';
 export { default as userEvent } from '@testing-library/user-event';
 export { customRender as render };
 export { renderHook, act } from '@testing-library/react-hooks';
-export { createServer as setupServer };
