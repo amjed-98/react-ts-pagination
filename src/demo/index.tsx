6459dd8bf03a3b3884d2266fda185d86cdd6f8a6
= <=>
2022-11-21 15:33:20 +0200

    change Without_usePagination_hook component name to Without_hooks

--- src/demo/index.tsx
+++ src/demo/index.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import {
   // With_usePagination_Hook,
-  // Without_usePagination_Hook,
+  // Without_hooks,
   With_userServerPagination_Hook,
 } from '@/demo/Components';
 import './index.css';
@@ -10,8 +10,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     {/* <With_usePagination_Hook /> */}
-    {/* <Without_usePagination_Hook /> */}
-
+    {/* <Without_hooks /> */}
     <With_userServerPagination_Hook />
   </StrictMode>,
 );
